fb4b802092b048a228999d5b049934862abdced7
Chris Aljoudi <chris@chrismatic.io>
2018-05-26 17:35:56 -0600

    Replaced swipe gestures with facial expression recognition.

--- BouncyBear/Game Scene/GameScene.swift
+++ BouncyBear/Game Scene/GameScene.swift
@@ -8,6 +8,7 @@
 
 import SpriteKit
 import GameplayKit
+import ARKit
 
 enum Direction {
     case up
@@ -25,11 +26,13 @@ class GameScene: SKScene {
     public var bearType: BearType?
     private var bear: Bear!
     
-    // swiping state
-    private var moveAmtY: CGFloat = 0
-    private let minimum_detect_distance: CGFloat = 100
-    private var initialPosition: CGPoint = CGPoint.zero
-    private var initialTouch: CGPoint = CGPoint.zero
+    // face tracking params
+    let smileThreshold = 0.65
+    let jawThreshold = 0.7
+    
+    // face tracking state
+    var smileIsCurrentlyThere = false
+    var jawIsCurrentlyOpen = false
     
     override func didMove(to view: SKView) {
         backgroundColor = .black
@@ -57,6 +60,7 @@ class GameScene: SKScene {
             currentStep = firstStep
         }
         
+        initFaceTracking()
     }
     
     // make the bear jump to the step in the direction provided
@@ -90,28 +94,74 @@ class GameScene: SKScene {
     }
 }
 
-// gestures and interactions
-extension GameScene {
+// Face tracking stuff
+extension GameScene: ARSCNViewDelegate {
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-            initialTouch = touch.location(in: self.scene!.view)
-            moveAmtY = 0
-            initialPosition = self.position
+    /// Initializes face tracking
+    func initFaceTracking() {
+        let configuration = ARFaceTrackingConfiguration()
+        
+        if let hostView = self.view {
+            let sceneView = ARSCNView(frame: hostView.bounds)
+            sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+            sceneView.isHidden = true
+            sceneView.delegate = self
+            
+            hostView.addSubview(sceneView)
         }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-            let movingPoint: CGPoint = touch.location(in: self.scene!.view)
-            moveAmtY = movingPoint.y - initialTouch.y
+    /// Gets called when there are updates to the face 'mesh'
+    public func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        
+        guard let faceAnchor = anchor as? ARFaceAnchor else {
+            return
+        }
+        
+        let smileIsThere = checkSmile(faceAnchor: faceAnchor, threshold: smileThreshold)
+        let jawIsOpen = checkJaw(faceAnchor: faceAnchor, threshold: jawThreshold)
+        
+        if smileIsCurrentlyThere != smileIsThere {
+            smileIsCurrentlyThere = smileIsThere
+            if smileIsThere {
+                jump(inDirection: .up)
+            }
+        }
+        else if jawIsCurrentlyOpen != jawIsOpen {
+            jawIsCurrentlyOpen = jawIsOpen
+            if jawIsOpen {
+                jump(inDirection: .down)
+            }
         }
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if fabs(moveAmtY) > minimum_detect_distance {
-            let direction: Direction = (moveAmtY < 0 ? .up : .down)
-            jump(inDirection: direction)
+    /// Checks for a smile given a faceAnchor.
+    ///
+    /// - Parameter faceAnchor: the face anchor to check for a smile.
+    /// - Parameter threshold: the the threshold of the smile coefficient.
+    /// - Returns: true or false, indicating whether a smile was detected.
+    func checkSmile(faceAnchor: ARFaceAnchor, threshold: Double) -> Bool {
+        let blendShapes = faceAnchor.blendShapes
+        
+        if let smileLeftValue = blendShapes[.mouthSmileLeft], let smileRightValue = blendShapes[.mouthSmileRight] {
+            return ((smileLeftValue.doubleValue + smileRightValue.doubleValue) / 2.0) >= threshold
         }
+        
+        return false
+    }
+    
+    /// Checks whether the jaw is open given a faceAnchor.
+    ///
+    /// - Parameter faceAnchor: the face anchor to check for an open jaw.
+    /// - Parameter threshold: the the threshold of the jaw-open coefficient.
+    /// - Returns: true or false, indicating whether an open jaw was detected.
+    func checkJaw(faceAnchor: ARFaceAnchor, threshold: Double) -> Bool {
+        let blendShapes = faceAnchor.blendShapes
+        
+        if let jawOpenValue = blendShapes[.jawOpen] {
+            return jawOpenValue.doubleValue >= threshold
+        }
+        
+        return false
     }
 }
